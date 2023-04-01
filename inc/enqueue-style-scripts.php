2fd6dd06037fe9efe69926794d93a5117f1fec44
Daniel Rojas <danielrojas243@gmail.com>
2023-04-01 17:18:52 -0500

    upadted version

--- inc/enqueue-style-scripts.php
+++ inc/enqueue-style-scripts.php
@@ -1,6 +1,6 @@
 <?php
 
-  define('CSS_VERSION', '1.1.0');
+  define('VERSION', '1.1.0');
 
   function load_swiper_on_specific_pages() {
     global $post;
@@ -18,23 +18,23 @@
     $isHome = is_front_page() || is_home();
 
     // Styles
-    wp_enqueue_style('mainCss',  CSS_BASE . 'main.css', array(), CSS_VERSION );
+    wp_enqueue_style('mainCss',  CSS_BASE . 'main.css', array(), VERSION);
 
     if ($isHome) {
-      wp_enqueue_style('homeCss',  CSS_BASE . 'home.css', array(), CSS_VERSION );
+      wp_enqueue_style('homeCss',  CSS_BASE . 'home.css', array(), VERSION);
     } else if (!empty($post) && $post->post_type == 'page') {
       if ($post->post_name == 'nosotros') {
-        wp_enqueue_style('aboutCss',  CSS_BASE . 'about.css', array(), CSS_VERSION );
+        wp_enqueue_style('aboutCss',  CSS_BASE . 'about.css', array(), VERSION);
       } else if ($post->post_name == 'locales-comerciales') {
-        wp_enqueue_style('bussinesCss',  CSS_BASE . 'bussines.css', array(), CSS_VERSION );
+        wp_enqueue_style('bussinesCss',  CSS_BASE . 'bussines.css', array(), VERSION);
       } else if ($post->post_name == 'contacto') {
-        wp_enqueue_style('contactCss',  CSS_BASE . 'contact.css', array(), CSS_VERSION );
+        wp_enqueue_style('contactCss',  CSS_BASE . 'contact.css', array(), VERSION);
       } 
     }
 
     // Scripts
     wp_enqueue_script('jqueryJs', JS_BASE . 'libs/jquery.min.js', array(), '3.6.4', true);
-    wp_enqueue_script('mainJs', JS_BASE . 'main.js', array('jqueryJs'), CSS_VERSION, true );
+    wp_enqueue_script('mainJs', JS_BASE . 'main.js', array('jqueryJs'), VERSION, true);
   }
   add_action('wp_enqueue_scripts', 'enqueue_style_scripts');
 ?>
